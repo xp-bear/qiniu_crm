edea8295140425801b7651b42abb20b976765ebb
想走过亚洲的熊 <1693889638@qq.com>
2023-02-03 10:46:31 +0800

    区分开发环境与生产环境。

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,9 @@
-let baseUrl = "http://127.0.0.1:7777/";
+let baseUrl = "";
+if (process.env.NODE_ENV == "development") {
+  // 开发环境
+  baseUrl = "http://localhost:7777";
+} else {
+  // 打包环境
+  baseUrl = "http://150.158.21.251:7777";
+}
 export { baseUrl };
